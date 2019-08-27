c42b27505bd743f2c929002fdd0d49288d5f06f3
Ben Falconer <ben@falconers.me.uk>
2019-08-27 23:37:31 +0100

    feature: Add a few more transforms

--- ctf-dash.js
+++ ctf-dash.js
@@ -10,18 +10,67 @@ function fromBaseTransform(base, baseName) {
 
 	return {
 		name: `From ${baseName}`,
-		validity: input => {
-			return input !== '' && _.every(_.toUpper(input), c => _.includes(digits, c))
-		},
+		validity: input => input !== '' && _.every(_.toUpper(input), c => _.includes(digits, c)),
 		transform: input => parseInt(input, base).toString(),
 	};
 }
 
+function toBaseTransform(base, baseName) {
+	const digits = ['0','1','2','3','4','5','6','7','8','9'];
+	return {
+		name: `To ${baseName}`,
+		validity: input => input !== '' && _.every(input, c => _.includes(digits, c)),
+		transform: input => parseInt(input, 10).toString(base),
+	}
+}
+
+const braille = {
+	' ': '⠀', '_': '⠸', '-': '⠤', ',': '⠠', ';': '⠰', ':': '⠱', '!': '⠮', '?': '⠹', '.': '⠨', '(': '⠷', '[': '⠪', '@': '⠈', '*': '⠡', '/': '⠌', '\'': '⠄', '\"': '⠐', '\\': '⠳', '&': '⠯', '%': '⠩', '^': '⠘', '+': '⠬', '<': '⠣', '>': '⠜', '$': '⠫',
+	'0': '⠴', '1': '⠂', '2': '⠆', '3': '⠒', '4': '⠲', '5': '⠢', '6': '⠖', '7': '⠶', '8': '⠦', '9': '⠔',
+	'A': '⠁', 'B': '⠃', 'C': '⠉', 'D': '⠙', 'E': '⠑', 'F': '⠋', 'G': '⠛', 'H': '⠓', 'I': '⠊', 'J': '⠚', 'K': '⠅', 'L': '⠇', 'M': '⠍', 'N': '⠝', 'O': '⠕', 'P': '⠏', 'Q': '⠟', 'R': '⠗', 'S': '⠎', 'T': '⠞', 'U': '⠥', 'V': '⠧', 'W': '⠺', 'X': '⠭', 'Z': '⠵',
+	']': '⠻', '#': '⠼', 'Y': '⠽', ')': '⠾', '=': '⠿'
+};
+
 const transforms = [
 	fromBaseTransform(2,  'binary'),
 	fromBaseTransform(8,  'octal'),
-	fromBaseTransform(10, 'decimal'),
 	fromBaseTransform(16, 'hex'),
+	toBaseTransform(2,  'binary'),
+	toBaseTransform(8,  'octal'),
+	toBaseTransform(16, 'hex'),
+	{
+		name: 'ASCII values',
+		validity: input => input !== '',
+		transform: input => _.map(input, c => c.charCodeAt(0).toString()).join(' '),
+	},
+	{
+		name: 'Strip spaces',
+		// NOTE: We specifically include a blank braille, which doesn't technically count as whitespace
+		// https://en.wikipedia.org/wiki/Trimming_(computer_programming)#Non-space_blanks
+		validity: input => /[\s⠀]/.test(input),
+		transform: input => _.replace(input, /[\s⠀]/g,''),
+	},
+	{
+		name: 'Index alphabet',
+		validity: input => _(input)
+			.split(' ')
+			.map(n => parseInt(n, 10))
+			.every(n => (n <= 26 && n > 0)),
+		transform: input => _(input)
+			.split(' ')
+			.map(n => parseInt(n, 10))
+			.map(n => String.fromCharCode('a'.charCodeAt(0) - 1 + n))
+			.join(''),
+	},
+	{
+		name: 'To braile',
+		validity: input => input !== '' && _.every(input, c => braille[c.toUpperCase()] !== undefined),
+		transform: input => {
+			return _(input)
+				.map(c => braille[c.toUpperCase()])
+				.join('');
+		},
+	},
 ];
 
 const maxDepth = 3;
